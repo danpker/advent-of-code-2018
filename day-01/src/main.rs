7bb9d7bf14310189f3914111d9f63d19182f170c
Daniel Parker <danpker@gmail.com>
2018-12-03 16:14:27 +0000

    Add part 2 answer for day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,14 +1,40 @@
-use std::io::{self, BufRead};
+use std::io::{self, Read};
+use std::collections::HashSet;
 
-fn main() {
-    let stdin = io::stdin();
-    let mut frequency = 0;
+fn part1(input_lines: &str) {
+    let mut frequency: i64 = 0;
 
-    for line in stdin.lock().lines() {
-        let read_line = line.unwrap();
-        let parsed_line = read_line.parse::<i64>().unwrap();
+    for line in input_lines.lines() {
+        let parsed_line = line.parse::<i64>().unwrap();
         frequency += parsed_line;
     }
 
-    println!("{}", frequency);
+    println!("Part 1 Answer: {}", frequency);
+}
+
+fn part2(input_lines: &str) {
+    let mut frequency: i64 = 0;
+    let mut frequencies = HashSet::new();
+
+    frequencies.insert(frequency);
+
+    loop {
+        for line in input_lines.lines() {
+            let parsed_line = line.parse::<i64>().unwrap();
+            frequency += parsed_line;
+
+            if !frequencies.insert(frequency) {
+                println!("Part 2 Answer: {}", frequency);
+                return
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    part1(&input);
+    part2(&input);
 }
